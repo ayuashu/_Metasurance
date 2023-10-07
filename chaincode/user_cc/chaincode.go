51b104244a16cce00d134256d45f2ad61cd63917
aritroCoder <ogocodinggod@gmail.com>
2023-10-07 13:52:23 +0530

    updated user register/login chaincode
    Signed-off-by: aritroCoder <ogocodinggod@gmail.com>

--- chaincode/user_cc/chaincode.go
+++ chaincode/user_cc/chaincode.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"os/exec"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
@@ -12,6 +13,7 @@ type User struct {
 	Email    string `json:"email"`
 	Phone    string `json:"phone"`
 	Password string `json:"password"`
+	UniqueID string `json:"uniqueID"`
 }
 
 type Chaincode struct {
@@ -24,32 +26,41 @@ func (t *Chaincode) Init(stub shim.ChaincodeStubInterface) peer.Response {
 func (t *Chaincode) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	function, args := stub.GetFunctionAndParameters()
 
-	if function == "registerUser" {
-		return t.registerUser(stub, args)
+	if function == "register" {
+		return t.register(stub, args)
 	} else if function == "readUserProfile" {
 		return t.readUserProfile(stub, args)
-	} else if function == "checkUser" {
-		return t.checkUser(stub, args)
+	} else if function == "checkUserExists" {
+		return t.checkUserExists(stub, args)
+	} else if function == "login" {
+		return t.login(stub, args)
 	}
 
-	return shim.Error("Invalid function name. Expecting 'registerUser', 'readUserProfile', or 'checkUser'")
+	return shim.Error("Invalid function name. Expecting 'register', 'readUserProfile', or 'checkUser'")
 }
 
-func (t *Chaincode) registerUser(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+func (t *Chaincode) register(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 4: name, email, phone, password")
 	}
+
+	newUUID, err := exec.Command("uuidgen").Output() 
+    if err != nil { 
+        return shim.Error("Cannot generate uuid") 
+    }
 
 	name := args[0]
 	email := args[1]
 	phone := args[2]
 	password := args[3]
+	uniqueID := string(newUUID)
 
 	user := User{
 		Name:     name,
 		Email:    email,
 		Phone:    phone,
 		Password: password,
+		UniqueID: uniqueID,
 	}
 
 	userJSON, err := json.Marshal(user)
@@ -62,7 +73,7 @@ func (t *Chaincode) registerUser(stub shim.ChaincodeStubInterface, args []string
 		return shim.Error("Failed to register user")
 	}
 
-	return shim.Success([]byte("User registered successfully"))
+	return shim.Success([]byte("The unique id of this user is: "+ uniqueID))
 }
 
 func (t *Chaincode) readUserProfile(stub shim.ChaincodeStubInterface, args []string) peer.Response {
@@ -84,7 +95,28 @@ func (t *Chaincode) readUserProfile(stub shim.ChaincodeStubInterface, args []str
 	return shim.Success(userJSON)
 }
 
-func (t *Chaincode) checkUser(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+func (t *Chaincode) checkUserExists(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 2: email, password")
+	}
+	userid := args[0]
+
+	userJSON, err := stub.GetState(userid)
+	if err != nil {
+		return shim.Error("Failed to check user")
+	}
+	var user User
+	err = json.Unmarshal(userJSON, &user)
+	if err != nil {
+		return shim.Error("Error unmarshaling user data")
+	}
+	if user.Email != "" {
+		return shim.Success([]byte("User exists"))
+	}
+	return shim.Success([]byte("User does not exist"))
+}
+
+func (t *Chaincode) login(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2: email, password")
 	}
@@ -111,5 +143,10 @@ func (t *Chaincode) checkUser(stub shim.ChaincodeStubInterface, args []string) p
 		return shim.Error("Invalid password")
 	}
 
-	return shim.Success([]byte("User exists and password is correct"))
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return shim.Error("Error marshaling user data")
+	}
+
+	return shim.Success(userBytes)
 }
