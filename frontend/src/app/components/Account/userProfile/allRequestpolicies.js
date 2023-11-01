229f5a054311725bc76e3f75d40f6682487ea6a1
aritroCoder <ogcodinggod@gmail.com>
2023-11-02 01:02:34 +0530

    added some API integration
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- frontend/src/app/components/Account/userProfile/allRequestpolicies.js
+++ frontend/src/app/components/Account/userProfile/allRequestpolicies.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react'
 
-const HOST = "http://localhost:3000";
+const HOST = 'http://localhost:3000'
 
 const AllRequestPolicyCard = ({ assetid, rendered }) => {
-    const [policyCompanies, setPolicyCompanies] = useState([]);
-    const cardRef = useRef(null);
+    const [policyCompanies, setPolicyCompanies] = useState([])
+    const cardRef = useRef(null)
 
     const handleRequestPolicy = async (assetid, policyid) => {
         try {
             const response = await fetch(`${HOST}/api/user/policy/request`, {
-                method: "POST",
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -17,80 +17,150 @@ const AllRequestPolicyCard = ({ assetid, rendered }) => {
                     policyid,
                     assetid,
                 }),
-            });
+            })
             if (response.ok) {
                 // Successfully purchased the policy, you can handle the response or perform any necessary action.
-                console.log('Policy purchase was successful');
+                console.log('Policy purchase was successful')
             } else {
-                console.error('Failed to purchase policy');
+                console.error('Failed to purchase policy')
             }
         } catch (error) {
-            console.error('Error purchasing policy', error);
+            console.error('Error purchasing policy', error)
         }
-    };
+    }
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 if (rendered) {
                     // Fetch data only if the component has been rendered
-                    const response = await fetch(`${HOST}/api/user/policy/viewall`, {
-                        method: "GET",
-                        credentials: "include",
-                    });
+                    const response = await fetch(
+                        `${HOST}/api/user/policy/viewall`,
+                        {
+                            method: 'GET',
+                            credentials: 'include',
+                        },
+                    )
                     if (response.ok) {
-                        const data = await response.json();
-                        setPolicyCompanies(data.reply.policyCompanies);
-                        console.log(data); // Log the data
+                        const data = await response.json()
+                        setPolicyCompanies(data.reply.policyCompanies)
+                        console.log(data) // Log the data
                     } else {
-                        console.error('Failed to fetch policy data');
+                        console.error('Failed to fetch policy data')
                     }
                 }
             } catch (error) {
-                console.error('Error fetching policy data', error);
+                console.error('Error fetching policy data', error)
             }
-        };
+        }
 
-        fetchData(); // Call the fetchData function when the component mounts
-    }, [rendered]);
+        fetchData() // Call the fetchData function when the component mounts
+    }, [rendered])
 
     return (
         <div className="main-card-container">
-            {policyCompanies.map((company) => {
-                const { companyName, policies } = company;
+            {policyCompanies && policyCompanies.length > 0 ? (
+                policyCompanies.map((company) => {
+                    const { companyName, policies } = company
 
-                return (
-                    <div key={companyName}>
-                        {policies.map((policy) => {
-                            const { policyid, policyname, insurancetype, premiumamount, insurancecover } = policy;
+                    return (
+                        <div key={companyName}>
+                            {policies && policies.length > 0 ? (
+                                policies.map((policy) => {
+                                    const {
+                                        policyid,
+                                        policyname,
+                                        insurancetype,
+                                        premiumamount,
+                                        insurancecover,
+                                    } = policy
 
-                            const policyId = `policy-${policyid}`;
+                                    const policyId = `policy-${policyid}`
 
-                            return (
-                                <div className="card-container" style={{marginLeft: '100px', marginRight: '100px', border: '2px solid wheat'}} key={policyId}>
-                                    <div className="card" style={{backgroundColor:'black', color: 'wheat'}} ref={cardRef}>
-                                        <div className="card-body">
-                                            <span className="card-title" style={{fontSize: '40px'}}>{policyname}</span>
-                                            <span className="card-author">{companyName}</span>
-                                            <ul>
-                                                <li><b>Insurance Type:</b> {insurancetype}</li>
-                                                <li><b>Insurance Cover:</b> {insurancecover}</li>
-                                                <li><b>Premium Amount:</b> {premiumamount}</li>
-                                            </ul>
+                                    return (
+                                        <div
+                                            className="card-container"
+                                            style={{
+                                                marginLeft: '100px',
+                                                marginRight: '100px',
+                                                border: '2px solid wheat',
+                                            }}
+                                            key={policyId}
+                                        >
+                                            <div
+                                                className="card"
+                                                style={{
+                                                    backgroundColor: 'black',
+                                                    color: 'wheat',
+                                                }}
+                                                ref={cardRef}
+                                            >
+                                                <div className="card-body">
+                                                    <span
+                                                        className="card-title"
+                                                        style={{
+                                                            fontSize: '40px',
+                                                        }}
+                                                    >
+                                                        {policyname}
+                                                    </span>
+                                                    <span className="card-author">
+                                                        {companyName}
+                                                    </span>
+                                                    <ul>
+                                                        <li>
+                                                            <b>
+                                                                Insurance Type:
+                                                            </b>{' '}
+                                                            {insurancetype}
+                                                        </li>
+                                                        <li>
+                                                            <b>
+                                                                Insurance Cover:
+                                                            </b>{' '}
+                                                            {insurancecover}
+                                                        </li>
+                                                        <li>
+                                                            <b>
+                                                                Premium Amount:
+                                                            </b>{' '}
+                                                            {premiumamount}
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                                <div>
+                                                    <button
+                                                        className="card-tag"
+                                                        onClick={() =>
+                                                            handleRequestPolicy(
+                                                                assetid,
+                                                                policyid,
+                                                            )
+                                                        }
+                                                    >
+                                                        Purchase
+                                                    </button>
+                                                </div>
                                             </div>
-                                            <div>
-                                                <button className='card-tag' onClick={() => handleRequestPolicy(assetid , policyid)}>Purchase</button>
-                                            </div>
-                                        
-                                    </div>
+                                        </div>
+                                    )
+                                })
+                            ) : (
+                                <div className="text-white text-2xl p-5">
+                                    Polcies registed for this asset will show
+                                    here
                                 </div>
-                            );
-                        })}
-                    </div>
-                );
-            })}
+                            )}
+                        </div>
+                    )
+                })
+            ) : (
+                <div className="text-white text-2xl p-5">
+                    Polcies registed for this asset will show here
+                </div>
+            )}
         </div>
-    );
-};
+    )
+}
 
-export default AllRequestPolicyCard;
+export default AllRequestPolicyCard
--- frontend/src/app/components/Account/userProfile/assetCard.js
+++ frontend/src/app/components/Account/userProfile/assetCard.js
@@ -1,99 +1,124 @@
-import React, { useState, useEffect } from 'react';
-import AllRequestPolicyCard from './allRequestpolicies';
+import React, { useState, useEffect } from 'react'
+import AllRequestPolicyCard from './allRequestpolicies'
 
-const HOST = "http://localhost:3000";
+const HOST = 'http://localhost:3000'
 
-
-const AssetCard = ({username}) => {
-    const [assets, setAssets] = useState([]);
-    const [showAllPolicies, setShowAllPolicies] = useState(false);
-    const [selectedAssetID, setSelectedAssetID] = useState(null);
+const AssetCard = ({ username }) => {
+    const [assets, setAssets] = useState([])
+    const [showAllPolicies, setShowAllPolicies] = useState(false)
+    const [selectedAssetID, setSelectedAssetID] = useState(null)
 
     const handleDelete = async (assetID) => {
         try {
             const response = await fetch(`${HOST}/api/user/asset/delete`, {
-                method: "DELETE",
+                method: 'DELETE',
                 credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ assetID }), // Send assetID in the request body
-            });
-    
+                body: JSON.stringify({ assetid: assetID }), // Send assetID in the request body
+            })
+
             if (response.ok) {
                 // If the delete request is successful, update the list of assets
-                setAssets((prevAssets) => prevAssets.filter((asset) => asset.assetID !== assetID));
+                setAssets((prevAssets) =>
+                    prevAssets.filter((asset) => asset.assetID !== assetID),
+                )
             } else {
-                console.error('Failed to delete the asset');
+                console.error('Failed to delete the asset')
             }
         } catch (error) {
-            console.error('Error deleting the asset', error);
+            console.error('Error deleting the asset', error)
         }
-    };
-    
+    }
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch(`${HOST}/api/user/asset/get`, {
-                    method: "GET",
-                    credentials: "include",
-                });
+                    method: 'GET',
+                    credentials: 'include',
+                })
 
                 if (response.ok) {
-                    const data = await response.json();
-                    setAssets(data.reply.assets);
+                    const data = await response.json()
+                    setAssets(data.reply.assets)
                     console.log(data)
                 } else {
-                    console.error('Failed to fetch assets data');
+                    console.error('Failed to fetch assets data')
                 }
             } catch (error) {
-                console.error('Error fetching assets data', error);
+                console.error('Error fetching assets data', error)
             }
-        };
+        }
 
-        fetchData();
-    }, []);
+        fetchData()
+    }, [])
 
     const handleRequestPolicy = (assetID) => {
-        setSelectedAssetID(assetID);
-        setShowAllPolicies(true);
-    };
+        setSelectedAssetID(assetID)
+        setShowAllPolicies((prev) => !prev)
+    }
 
     return (
         <>
             <div className="main-card--container">
-                {assets.map((asset) => {
-                    const { assetID, assetName, assetType, value, age } = asset;
-                    
-                    return (
-    
-                        <div className="card-container" key={assetID}>
-                            <div className="card">
-                                <div className="card-body">
-                                    <h2 className="card-title">{assetName}</h2>
-                                    <ul>
-                                        <li>Asset Type: {assetType}</li>
-                                        <li>Value: {value}</li>
-                                        <li>Age: {age}</li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <button className='card-tag' onClick={() => handleRequestPolicy(assetID)}>Get Policies</button>
-                                    <button className='card-tag' onClick={() => handleDelete(assetID)}>Delete</button>
+                {assets && assets.length > 0 ? (
+                    assets.map((asset) => {
+                        const { assetID, assetName, assetType, value, age } =
+                            asset
+
+                        return (
+                            <div className="card-container" key={assetID}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h2 className="card-title">
+                                            {assetName}
+                                        </h2>
+                                        <ul>
+                                            <li>Asset Type: {assetType}</li>
+                                            <li>Value: {value}</li>
+                                            <li>Age: {age}</li>
+                                        </ul>
+                                    </div>
+                                    <div>
+                                        <button
+                                            className="card-tag"
+                                            onClick={() =>
+                                                handleRequestPolicy(assetID)
+                                            }
+                                        >
+                                            Get Policies
+                                        </button>
+                                        <button
+                                            className="card-tag"
+                                            onClick={() =>
+                                                handleDelete(assetID)
+                                            }
+                                        >
+                                            Delete
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        )
+                    })
+                ) : (
+                    <div className="text-white text-2xl p-5">
+                        Add some assets to show up here
+                    </div>
+                )}
             </div>
 
             {/* Render the AllPolicies component conditionally */}
-            {showAllPolicies && <AllRequestPolicyCard assetID={selectedAssetID} rendered={true} />}
+            {showAllPolicies && (
+                <AllRequestPolicyCard
+                    assetID={selectedAssetID}
+                    rendered={true}
+                />
+            )}
         </>
-    );
-};
+    )
+}
 
-export default AssetCard;
-
-
+export default AssetCard
