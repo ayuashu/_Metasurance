c89a6460f62e66e7e45767e510d778f37d571ee5
aritroCoder <ogcodinggod@gmail.com>
2023-11-06 16:03:38 +0530

    fixed filter bug
    Signed-off-by: aritroCoder <ogcodinggod@gmail.com>

--- frontend/src/app/components/Account/page.js
+++ frontend/src/app/components/Account/page.js
@@ -1,62 +1,129 @@
-"use client"
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import Navigation from '../Navigation/page'
 import Footer from '../Footer/footer'
 import ProfileAnimation from '../PolicyAnimation/ProfileAnimation/page'
 import ProfileAnimation1 from '../PolicyAnimation/ProfileAnimation1/page'
 
+const HOST = 'http://localhost:3000'
+
 const Account = () => {
-  const router = useRouter()
-  const navigate = (location) => {
-    router.push(location)
-  }
+    const router = useRouter()
+    const navigate = (location) => {
+        router.push(location)
+    }
 
-  return (
-    <>
-      <div className="bg-slate-700 bg-blend-lighten hover:bg-blend-darken min-h-screen" >
-        <Navigation />
-        <div className="grid grid-cols-3 gap-4 min-h-screen px-10 py-10">
-          <div className="..." style={{ height: '80vh' }}>
-            <div className="w-full max-w-sm min-h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex flex-col items-center pb-10 pt-20">
-                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/Images/pic.jpeg" alt="" />
-                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">!!!Hurray!!!</h5>
-                <h3 className="mb-1 text-xl font-medium text-gray-800 dark:text-white">Time to Explore</h3>
-                <span className="text-sm text-gray-500 dark:text-gray-400">User/Company</span>
-              </div>
-            </div>
-          </div>
-          <div className="col-span-2 ...">
-            <div className="flex items-center justify-end pt-5 gap-4">
-              <button
-                onClick={() => navigate('/components/Account/userRegistration')}
-                className="h-20 px-11 text-indigo-100 text-lg transition-colors duration-150 bg-slate-600 rounded-full focus:shadow-outline hover:bg-slate-900">
-                <b>Use Service<p>(USER)</p></b>
-              </button>
-              <button
-                onClick={() => navigate('/components/Account/companyRegistration')}
-                className="h-20 px-11 text-indigo-100 text-lg transition-colors duration-150 bg-slate-600 rounded-full focus:shadow-outline hover:bg-slate-900">
-                <b>Give Service<p>(COMPANY)</p></b>
-              </button>
-            </div>
-            <div className='grid grid-cols-2 gap-20 pt-20'>
-              <div className='profile-box' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <ProfileAnimation />
-              </div>
-              <div className='profile-box' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '80px'}}>
-                <ProfileAnimation1 />
-              </div>
-            </div>
+    // if user had auth cookie, directly send to respective profile
+    useEffect(() => {
+        async function checkAuth() {
+            let response = await fetch(`${HOST}/api/user/readProfile`, {
+                method: 'GET',
+                credentials: 'include',
+            })
+            let data = await response.json()
+            if (response.status === 200 && !response.error) {
+                navigate('/components/Account/userProfile')
+            }
 
+            response = await fetch(`${HOST}/api/company/readProfile`, {
+                method: 'GET',
+                credentials: 'include',
+            })
+            data = await response.json()
+            if (response.status === 200 && !response.error) {
+                navigate('/components/Account/companyProfile')
+            }
+        }
+        try {
+            checkAuth()
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
 
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="bg-slate-700 bg-blend-lighten hover:bg-blend-darken min-h-screen">
+                <Navigation />
+                <div className="grid grid-cols-3 gap-4 min-h-screen px-10 py-10">
+                    <div className="..." style={{ height: '80vh' }}>
+                        <div className="w-full max-w-sm min-h-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                            <div className="flex flex-col items-center pb-10 pt-20">
+                                <img
+                                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                                    src="/Images/pic.jpeg"
+                                    alt=""
+                                />
+                                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                                    Hurray !
+                                </h5>
+                                <h3 className="mb-1 text-xl font-medium text-gray-800 dark:text-white">
+                                    Time to Explore
+                                </h3>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    User/Company
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-2 ...">
+                        <div className="flex items-center justify-end pt-5 gap-4">
+                            <button
+                                onClick={() =>
+                                    navigate(
+                                        '/components/Account/userRegistration',
+                                    )
+                                }
+                                className="h-20 px-11 text-indigo-100 text-lg transition-colors duration-150 bg-slate-600 rounded-full focus:shadow-outline hover:bg-slate-900"
+                            >
+                                <b>
+                                    Use Service<p>(USER)</p>
+                                </b>
+                            </button>
+                            <button
+                                onClick={() =>
+                                    navigate(
+                                        '/components/Account/companyRegistration',
+                                    )
+                                }
+                                className="h-20 px-11 text-indigo-100 text-lg transition-colors duration-150 bg-slate-600 rounded-full focus:shadow-outline hover:bg-slate-900"
+                            >
+                                <b>
+                                    Give Service<p>(COMPANY)</p>
+                                </b>
+                            </button>
+                        </div>
+                        <div className="grid grid-cols-2 gap-20 pt-20">
+                            <div
+                                className="profile-box"
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}
+                            >
+                                <ProfileAnimation />
+                            </div>
+                            <div
+                                className="profile-box"
+                                style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    marginTop: '80px',
+                                }}
+                            >
+                                <ProfileAnimation1 />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 export default Account
--- frontend/src/app/components/Account/userProfile/Services/allPolicies.js
+++ frontend/src/app/components/Account/userProfile/Services/allPolicies.js
@@ -1,100 +1,149 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-const HOST = 'http://localhost:3000';
+const HOST = 'http://localhost:3000'
 
 const AllPolicyCard = ({ selectedInsuranceType, selectedCompanyName }) => {
-  const [policyCompanies, setPolicyCompanies] = useState([]);
-  const [filteredPolicyCompanies, setFilteredPolicyCompanies] = useState([]);
+    const [policyCompanies, setPolicyCompanies] = useState([])
+    const [filteredPolicyCompanies, setFilteredPolicyCompanies] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${HOST}/api/user/policy/viewall`, {
-          method: 'GET',
-          credentials: 'include',
-        });
-        if (response.ok) {
-          const data = await response.json();
-          setPolicyCompanies(data.reply.policyCompanies);
-          console.log('Fetched data:', data);
-        } else {
-          console.error('Failed to fetch policy data');
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    `${HOST}/api/user/policy/viewall`,
+                    {
+                        method: 'GET',
+                        credentials: 'include',
+                    },
+                )
+                if (response.ok) {
+                    const data = await response.json()
+                    setPolicyCompanies(data.reply.policyCompanies)
+                    console.log('Fetched data:', data)
+                } else {
+                    console.error('Failed to fetch policy data')
+                }
+            } catch (error) {
+                console.error('Error fetching policy data', error)
+            }
         }
-      } catch (error) {
-        console.error('Error fetching policy data', error);
-      }
-    }
 
-    fetchData();
-  }, []);
+        fetchData()
+    }, [])
 
-  useEffect(() => {
-    // Filter policy companies based on selectedInsuranceType and selectedCompanyName
-    if (selectedInsuranceType || selectedCompanyName) {
-      const filteredCompanies = policyCompanies.filter((company) => {
-        const policies = company.policies;
-        return policies.some(
-          (policy) =>
-            (!selectedInsuranceType || policy.insurancetype === selectedInsuranceType) &&
-            (!selectedCompanyName || company.companyName === selectedCompanyName)
-        );
-      });
-      setFilteredPolicyCompanies(filteredCompanies);
-    } else {
-      // If no filters are selected, show all policy companies
-      setFilteredPolicyCompanies(policyCompanies);
-    }
-  }, [selectedInsuranceType, selectedCompanyName, policyCompanies]);
+    useEffect(() => {
+        setFilteredPolicyCompanies(policyCompanies)
+        if (selectedCompanyName.length > 0) {
+            setFilteredPolicyCompanies((prev) => {
+                return prev.filter(
+                    (company) => company.companyName === selectedCompanyName,
+                )
+            })
+        }
 
-  return (
-    <div className="main-card-container">
-      {filteredPolicyCompanies.map((company) => {
-        const { companyName, policies } = company;
-        return (
-          <div key={companyName}>
-            {policies.map((policy) => {
-              const {
-                policyid,
-                policyname,
-                insurancetype,
-                premiumamount,
-                insurancecover,
-              } = policy;
+        if (selectedInsuranceType.length > 0) {
+            setFilteredPolicyCompanies((prev) => {
+                return prev.map((company) => {
+                    const filteredPolicies = company.policies.filter(
+                        (policy) =>
+                            policy.insurancetype === selectedInsuranceType,
+                    )
 
-              const policyId = `policy-${policyid}`;
+                    return { ...company, policies: filteredPolicies }
+                })
+            })
+        }
+    }, [selectedInsuranceType, selectedCompanyName, policyCompanies])
 
-              return (
-                <div className="card-container" key={policyId}>
-                  <div className="card">
-                    <div className="card-body">
-                      <span className="card-title">{policyname}</span>
-                      <span className="card-author">{companyName}</span>
-                      <table>
-                        <tbody> 
-                          <tr>
-                            <td><b>Insurance Type:</b></td>
-                            <td colSpan="3" style={{ paddingLeft: '20px' }}>{insurancetype}</td>
-                          </tr>
-                          <tr>
-                            <td><b>Insurance Cover:</b></td>
-                            <td colSpan="3" style={{ paddingLeft: '20px' }}>{insurancecover}</td>
-                          </tr>
-                          <tr>
-                            <td><b>Premium Amount:</b></td>
-                            <td colSpan="3" style={{ paddingLeft: '20px' }}>{premiumamount}</td>
-                          </tr>
-                        </tbody>
-                      </table>
+    return (
+        <div className="main-card-container">
+            {filteredPolicyCompanies.map((company) => {
+                const { companyName, policies } = company
+                return (
+                    <div key={companyName}>
+                        {policies.map((policy) => {
+                            const {
+                                policyid,
+                                policyname,
+                                insurancetype,
+                                premiumamount,
+                                insurancecover,
+                            } = policy
+
+                            const policyId = `policy-${policyid}`
+
+                            return (
+                                <div className="card-container" key={policyId}>
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <span className="card-title">
+                                                {policyname}
+                                            </span>
+                                            <span className="card-author">
+                                                {companyName}
+                                            </span>
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <b>
+                                                                Insurance Type:
+                                                            </b>
+                                                        </td>
+                                                        <td
+                                                            colSpan="3"
+                                                            style={{
+                                                                paddingLeft:
+                                                                    '20px',
+                                                            }}
+                                                        >
+                                                            {insurancetype}
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <b>
+                                                                Insurance Cover:
+                                                            </b>
+                                                        </td>
+                                                        <td
+                                                            colSpan="3"
+                                                            style={{
+                                                                paddingLeft:
+                                                                    '20px',
+                                                            }}
+                                                        >
+                                                            {insurancecover}
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <b>
+                                                                Premium Amount:
+                                                            </b>
+                                                        </td>
+                                                        <td
+                                                            colSpan="3"
+                                                            style={{
+                                                                paddingLeft:
+                                                                    '20px',
+                                                            }}
+                                                        >
+                                                            {premiumamount}
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
-                  </div>
-                </div>
-              );
+                )
             })}
-          </div>
-        );
-      })}
-    </div>
-  );
+        </div>
+    )
 }
 
-export default AllPolicyCard;
+export default AllPolicyCard
